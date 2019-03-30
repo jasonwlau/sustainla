8816b6690c0e55eef569ae0d23d66115b4d8864b
Andrew Z. Kan <43661978+kandrewz@users.noreply.github.com>
2019-03-30 16:20:48 -0700

    item props
    added urls as a prop and make them disappear according to bin prop

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import Draggable from 'react-draggable';
-import logo from './bin.jpg';
+import plasticcup from './itemimages/plasticcup.jpg';
+import food from './itemimages/food.jpg';
+import pencil from './itemimages/pencil.jpg';
 
 class Item extends Component {
     constructor(props, context) {
@@ -12,14 +14,28 @@ class Item extends Component {
             }
         };
     }
+
     handleStop = (e, data) => {
-        if (data.x > 100 && data.y > 500) {
+      let down, left, up, right = 0;
+          if (this.props.bin == 'recycling')
+            {
+              down = -60; 
+              left = 96; 
+              up = 450; 
+              right = 259;
+            }
+            else
+            {
+              down = 500; left = 96; up = 400; right = 259;
+            }
+        if (data.x > down && data.y > left && data.y < up && data.x < right) {
            this.setState({
                position: {x: -999, y: -999}
            })
         }
         console.log(data.x);
         console.log(data.y);
+        console.log(this.props.url);
     }
     handleDrag = (e, data) => {
         const {x, y} = data;
@@ -31,7 +47,18 @@ class Item extends Component {
         console.log('Event: ', e);
         console.log('Data', data);
     }
+    getImage = () => {
+      let x = this.props.itemtype;
+      if (x == 'plasticcup')
+        return plasticcup;
+      else if (x == 'food')
+        return food;
+      else if (x == 'pencil')
+        return pencil;
+    }
   render() {
+    let image = this.getImage();
+    
     return (
         <Draggable
         axis = "both"
@@ -43,11 +70,11 @@ class Item extends Component {
         onDrag={this.handleDrag}
         onStop={this.handleStop}>
         <div className = "item">
-            <img id="layout" draggable="false" src={logo} />
+            <img id="layout" draggable="false" src={image} />
+            <p>{this.props.bin}</p>
         </div>
         </Draggable>
     );
   }
 }
-
 export default Item;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Bin from './Bin';
+import Item from './Item';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Bin />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
+ReactDOM.render(<Item bin='recycling' itemtype='plasticcup'/>, document.getElementById('item1'));
+ReactDOM.render(<Item bin='compost' itemtype='food'/> , document.getElementById('item2'));
+ReactDOM.render(<Item bin='landfill' itemtype='pencil'/>, document.getElementById('item3'));
+ReactDOM.render(<Bin />, document.getElementById('bin1'));
+ReactDOM.render(<Bin />, document.getElementById('bin2'));
+ReactDOM.render(<Bin />, document.getElementById('bin3'));
+// If you want your app to work offline and load fastfer, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();

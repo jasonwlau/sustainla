2a7d90a9f4691a5d28dfdd3825b09facffea2661
Jason Lau <jasonwla@usc.edu>
2019-03-30 14:51:19 -0700

    Add files via upload
    Edited handleDrag

--- src/Item.js
+++ src/Item.js
@@ -4,6 +4,29 @@ import Draggable from 'react-draggable';
 import logo from './bin.jpg';
 
 class Item extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = { 
+            position: {
+                x: 0, y: 0
+            }
+        };
+    }
+    handleStop = (e, data) => {
+        if (data.x > 100 && data.y > 500) {
+           this.setState({
+               position: {x: -999, y: -999}
+           })
+        }
+        console.log(data.x);
+        console.log(data.y);
+    }
+    handleDrag = (e, data) => {
+        const {x, y} = data;
+        this.setState({
+            position: {x:x, y:y}
+        })
+    }
     eventLogger = (e: MouseEvent, data: Object) => {
         console.log('Event: ', e);
         console.log('Data', data);
@@ -13,8 +36,8 @@ class Item extends Component {
         <Draggable
         axis = "both"
         handle = ".item"
-        defaultPosition={{x: 0, y: 0}}
-        position={null}
+        defaultPosition={null}
+        position={this.state.position}
         scale={1}
         onStart={this.handleStart}
         onDrag={this.handleDrag}
